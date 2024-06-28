e629951413d29093e850d7c379642e7ea592c7ef
Satish <43448483+andrasatish@users.noreply.github.com>
2024-06-28 12:49:35 +0530

    Create 01_variables.js

---
+++ 01_Session/01_variables.js
@@ -0,0 +1,19 @@
+//type of data -> number, string, boolean, arrays, object and date
+
+// variable -> var, let and const -> predefined keywords
+
+var num1 = 10; // number type
+console.log(num1); //10
+
+var sum = 10 + 20; // assign
+console.log(sum) //30
+
+sum = "Sudheer"; // reassignment
+console.log(sum);
+
+sum = [1,2,3]; // reassignment
+console.log(sum);
+
+// int num = 10;
+// num = 90;
+// num = "sudheer"
