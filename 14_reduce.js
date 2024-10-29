d351b438df89ec40e96fa6cc82ee8fa3f8bcd7da
Satish <43448483+andrasatish@users.noreply.github.com>
2024-10-29 17:56:15 +0530

    reduce one

--- 14_reduce.js
+++ 14_reduce.js
@@ -23,5 +23,58 @@ const uniqueValues = (input) => {
 console.log('UNIQUE VALUES :: ', uniqueValues(numbers));
 
 
-//SCENARIO-2 : sum of numbers
-const numArr = [10,20,30];
+//SCENARIO-2 : sum of numbers [10,20,30] -> 60
+const numArr = [10, 20, 30];
+const sumOfNumbers = (arr) => {
+    const sumOfNums = arr.reduce((a, c) => {
+        // console.log('sum of values', a, c)
+        // a = a+c;
+        a += c;
+        return a;
+    }, 0);
+    return sumOfNums;
+}
+
+console.log('SUM OF NUMBERS ::: ', sumOfNumbers(numArr));
+
+
+
+//SCENARIO-3 : count each character in the string. 
+
+/**
+ * str = "sudheer satish mohannnnn"  => { s: 2, u:1 }
+ */
+
+// obj = { name: 'sudheer' }  
+// obj.name // If exists : true, If not exists: false
+
+let str = "sudheer satish mohannnnn";
+
+const countEachChar = (str) => {
+    const eachCharCount = str.split('').reduce((a, c) => {
+        // if(a[c]){
+        //     a[c] +=  1;
+        // }else{
+        //     a[c] = 1;
+        // }
+        a[c] ? a[c] +=  1 : a[c] = 1;
+        return a;
+    }, { });
+    return eachCharCount;
+}
+console.log('Each Character Count ::: ', countEachChar(str))
+
+
+//SCENARIO-4 : Agg count based make wise
+const mobileSales = [
+    { location: 'Hyderabad', make: 'RealMe', count : 200 },
+    { location: 'Hyderabad', make: 'Nokia', count : 300 },
+    { location: 'Hyderabad', make: 'GT', count : 100 },
+    { location: 'Hyderabad', make: 'Nokia', count : 500 },
+    { location: 'Hyderabad', make: 'RealMe', count : 300 },
+] 
+
+
+
+
+
